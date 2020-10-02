df44768626f8df466f24f951383a320311ef5116
Frankmauro2000 <72162555+Frankmauro2000@users.noreply.github.com>
2020-10-02 14:35:58 -0500

    se crea el cambio

--- index.php
+++ index.php
@@ -2,34 +2,68 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
-    <title>Tiendas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
-
 <body>
     <header>
-    <img src="" alt="" srcset="">
-    
-    </header>
-    
-    <main>
-    <form action="">
-    <div class="">
-    <label>nombre</label>
-    <input type="text" name="nombre">
-    </div>
+        <h1>TIENDA.COM</h1>
+        
+    </header>  
+            <h1> <center> Registrese</center></h1>
+    <main class="bg-dark text-white">
+        <div class="container">
+             <div class="row justify-content-center">
+                 <div class="col-4">
+                         <form action="">
+                             <div class="form-group">
+                                    <label for="identi+">Identificacion</label>
+                                    <input type="number" class="form-control" id="identi" >
+                                    <small id="emailHelp" class="form-text text-muted"></small>
+                                 </div>
+                                 <hr>
 
-
-    </form>
+                                 <div class="form-group">
+                                    <label for="exampleInputEmail1">Nombre</label>
+                                    <input type="text" class="form-control" id="nombre" aria-describedby="emailHelp">
+                                    <small id="emailHelp" class="form-text text-muted"></small>
+                                 </div>
+                                 <hr>
+                                 <div class="form-group">
+                                    <label for="exampleInputEmail1">Apellido</label>
+                                    <input type="text" class="form-control" id="apellidos" aria-describedby="emailHelp">
+                                  
+                                 </div>
+                                 <hr>
+                                 <div class="form-group">
+                                    <label for="exampleInputEmail1">Edad</label>
+                                    <input type="number" class="form-control" id="edad" aria-describedby="emailHelp">
+                                    <small id="emailHelp" class="form-text text-muted"></small>
+                                 </div>
+                                 <hr>
+                                 <div class="form-group">
+                                    <label for="exampleInputEmail1">Telefono</label>
+                                    <input type="number" class="form-control" id="movil" aria-describedby="emailHelp">
+                                    <small id="emailHelp" class="form-text text-muted"></small>
+                                 </div>
+                                 <div class="form-group">
+                                    <label for="exampleInputEmail1">Direccion</label>
+                                    <input type="text" class="form-control" id="casa" aria-describedby="emailHelp">
+                                    <small id="emailHelp" class="form-text text-muted"></small>
+                                 </div>
+                                 <hr>
+                                 <button type="submit" class="btn btn-outline-success">Enviar</button>
+                                 
+    
+                            </form>
+        </div>
     </main>
-
-    <footer>
-    </footer>
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    
+     <footer>
+     </footer>
+     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script> 
 </body>
-
 </html>
